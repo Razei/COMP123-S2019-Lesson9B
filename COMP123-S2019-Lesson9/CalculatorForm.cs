88151c0f9b74efcea2485a4565d3d663446dea24
Jarod Lavine <jarodl@live.com>
2019-07-08 14:22:22 -0400

    Added shared event handler for calculator buttons

--- COMP123-S2019-Lesson9/CalculatorForm.cs
+++ COMP123-S2019-Lesson9/CalculatorForm.cs
@@ -12,9 +12,35 @@ namespace COMP123_S2019_Lesson9
 {
     public partial class CalculatorForm : Form
     {
+        /// <summary>
+        /// this is the constructor for the calculator form
+        /// </summary>
         public CalculatorForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// this is the event handler for all the calculator buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            var TheButton = sender as Button;
+
+            int buttonValue;
+            bool resultCondition = int.TryParse(TheButton.Text, out buttonValue);
+
+            if (resultCondition)
+            {
+                ResultLabel.Text += TheButton.Text;
+            }
+            else
+            {
+                ResultLabel.Text = "NaN";
+            }
+
         }
     }
 }
